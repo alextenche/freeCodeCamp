b842dca4630d1b415b5d75d0155561a51cfe4f17
Liviu Iancu <liviuiancu@gmx.com>
2017-01-15 12:00:50 +0200

    Cosmetic update
    Functionality is the same

--- algorithms/pairwise.js
+++ algorithms/pairwise.js
@@ -1,13 +1,9 @@
 function pairwise(arr, arg){
   var result = arr.reduce(function(accumulator, currentValue, currentIndex, initialArray) {
-    for (var i = currentIndex + 1; i < initialArray.length; i += 1) {
-      if(currentValue === undefined) {
-        break;
-      }
-      if (((currentValue + initialArray[i]) == arg) ) {
+    for (var i = currentIndex + 1; i < initialArray.length; i++) {
+      if (currentValue !== undefined && currentValue + initialArray[i] == arg) {
         accumulator += currentIndex + i;
-        initialArray[currentIndex] = undefined;
-        initialArray[i] = undefined;
+        currentIndex = initialArray[i] = undefined;
         break;
       }
     }
@@ -15,5 +11,4 @@ function pairwise(arr, arg){
   }, 0);
   return result;
 }
-
 pairwise([1, 1, 1], 2);
